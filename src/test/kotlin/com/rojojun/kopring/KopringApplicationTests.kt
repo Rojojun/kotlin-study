1a101b6ddb27fb641bf4d0d70d75a1c5c4d0f90d
rojojun <zoloman316@gmail.com>
2024-08-23 21:50:32 +0900

    initial

---
+++ src/test/kotlin/com/rojojun/kopring/KopringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rojojun.kopring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
