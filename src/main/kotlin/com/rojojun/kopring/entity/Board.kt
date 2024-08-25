3f13131e52304473fdebec46b129dc199a2ffbd5
rojojun <zoloman316@gmail.com>
2024-08-25 23:00:18 +0900

    Set Constructor

--- src/main/kotlin/com/rojojun/kopring/entity/Board.kt
+++ src/main/kotlin/com/rojojun/kopring/entity/Board.kt
@@ -1,10 +1,12 @@
 package com.rojojun.kopring.entity
 
 import jakarta.persistence.Entity
+import jakarta.persistence.Table
 
 @Entity
+@Table(name = "board")
 data class Board(
-    val title: String,
+    val title: String = "",
+    val content: String = ""
 ) : BaseEntity() {
-    Board ()
 }
