4ebed729bd504d073591df00c72827f8381fdbad
rojojun <zoloman316@gmail.com>
2024-09-07 23:01:57 +0900

    Kotlin :: securityconfig

--- src/main/kotlin/com/rojojun/kopring/config/SecurityConfig.kt
+++ src/main/kotlin/com/rojojun/kopring/config/SecurityConfig.kt
@@ -1,14 +1,21 @@
 package com.rojojun.kopring.config
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
-import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 class SecurityConfig {
+
+    @Bean
+    public fun webSecurityCustomizer(): WebSecurityCustomizer = WebSecurityCustomizer { webSecurity ->
+        webSecurity.ignoring().requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/health")
+    }
+
+    @Bean
+    public fun
 
     @Bean
     fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder();
